4d0945d3cb24d9182509bd8f62c66723837f68ab
AriAmaraldaSilva <33501131+AriAmaraldaSilva@users.noreply.github.com>
2017-11-29 19:08:17 -0200

    Versão 0.2 do sistema para controle de estoque

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Controle de Estoque</title>
+        <link href="bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <script src="bootstrap-3.3.7-dist/js/bootstrap.min.js" type="text/javascript"></script>
+    </head>
+    <body>
+        <div class="jumbotron text-center">
+            <h1>Controle de Estoque</h1>
+            <p>Aumoxerifado de Moto</p>
+        </div>    
+            <div class="container-fluid">
+                <div class="row">
+                    <div class=" col-sm-6 col-md-4 col-xs-push-4"> 
+                   <form class="form-horizontal">
+                    <div class="form-group">
+                      <label for="inputEmail3" class="col-sm-2 control-label">Usuario</label>
+                      <div class="col-sm-10">
+         <input type="email" class="form-control" id="inputEmail3" placeholder="Email ou usuario">
+                      </div>
+                    </div>
+                    <div class="form-group">
+                      <label for="inputPassword3" class="col-sm-2 control-label">Senha</label>
+                      <div class="col-sm-10">
+                <input type="password" class="form-control" id="inputPassword3" placeholder="Senha">
+                    </div>
+                    </div>
+                    <div class="form-group">
+                      <div class="col-sm-offset-2 col-sm-10">
+                          <button type="submit" class="btn btn-primary glyphicon glyphicon-user"> Login</button>
+                      </div>
+                    </div>
+                  </form>
+                    </div>
+                </div>
+            </div>
+              
+        
+        <?php
+        
+        ?>
+    </body>
+</html>
